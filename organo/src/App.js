985e5dd8bb01a335be39aad4db2c366f990ff1fc
Victor Portugal <victorportugalnobrega@gmail.com>
2025-08-16 18:28:27 -0300

    Criando o componente CampoTexto

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,9 +1,13 @@
-import Banner from './components/Banner/Banner';
+import Banner from './components/Banner'
+import CampoTexto from './components/CampoTexto'
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <CampoTexto label="Nome" placeholder="Digite o nome do personagem" />
+      <CampoTexto label="Cargo" placeholder="Digite o cargo do personagem" />
+      <CampoTexto label="Imagem" placeholder="Informe a URL da imagem" />
     </div>
   );
 }
