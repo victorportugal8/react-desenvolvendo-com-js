ba16a95a4ceef504180037d2989894a2edb2cd18
Victor Portugal <victorportugalnobrega@gmail.com>
2025-08-16 19:28:05 -0300

    Criando a lista suspensa

--- organo/src/components/Formulario/index.js
+++ organo/src/components/Formulario/index.js
@@ -1,7 +1,18 @@
 import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 
 const Formulario = () => {
+    const animes = [
+        'Attack on Titan',
+        'Dragon Ball',
+        'Hunter x Hunter',
+        'Jujutsu Kaisen',
+        'Naruto',
+        'One Piece',
+        'Solo Leveling'
+    ];
+
     return(
         <section className="formulario">
             <form>
@@ -9,6 +20,7 @@ const Formulario = () => {
                 <CampoTexto label="Nome" placeholder="Digite o nome do personagem" />
                 <CampoTexto label="Cargo" placeholder="Digite o cargo do personagem" />
                 <CampoTexto label="Imagem" placeholder="Informe a URL da imagem" />
+                <ListaSuspensa label="Anime" itens={animes} />
             </form>
         </section>
     )
