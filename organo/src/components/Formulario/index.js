191518d20587a2868fc675915706014aa21c7dc6
Victor Portugal <victorportugalnobrega@gmail.com>
2025-08-18 11:54:50 -0300

    Manipulando o input e controlando o estado da aplicação

--- organo/src/components/Formulario/index.js
+++ organo/src/components/Formulario/index.js
@@ -1,9 +1,10 @@
+import { useState } from 'react'
 import Botao from '../Botao'
 import CampoTexto from '../CampoTexto'
 import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 
-const Formulario = () => {
+const Formulario = () =>{
     const animes = [
         'Attack on Titan',
         'Dragon Ball',
@@ -12,7 +13,12 @@ const Formulario = () => {
         'Naruto',
         'One Piece',
         'Solo Leveling'
-    ];
+    ]
+
+    const [nome, setNome] = useState('')
+    const [cargo, setCargo] = useState('')
+    const [imagem, setImagem] = useState('')
+    const [anime, setAnime] = useState('')
 
     const aoSalvar = (evento) =>{
         evento.preventDefault()
@@ -22,10 +28,10 @@ const Formulario = () => {
         <section className="formulario">
             <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do personagem.</h2>
-                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite o nome do personagem" />
-                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite o cargo do personagem" />
-                <CampoTexto label="Imagem" placeholder="Informe a URL da imagem" />
-                <ListaSuspensa obrigatorio={true} label="Anime" itens={animes} />
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite o nome do personagem" valor={nome} aoAlterado={valor => setNome(valor)} />
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite o cargo do personagem" valor={cargo} aoAlterado={valor => setCargo(valor)} />
+                <CampoTexto label="Imagem" placeholder="Informe a URL da imagem" valor={imagem} aoAlterado={valor => setImagem(valor)} />
+                <ListaSuspensa obrigatorio={true} label="Anime" itens={animes} valor={anime} aoAlterado={valor => setAnime(valor)} />
                 <Botao>Criar Card</Botao>
             </form>
         </section>
