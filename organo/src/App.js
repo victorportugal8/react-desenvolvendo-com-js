0c1870e537d266b4a50e8685cb18fecabb839628
Victor Portugal <victorportugalnobrega@gmail.com>
2025-08-19 10:58:03 -0300

    Criando o componente Anime

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Banner from './components/Banner'
 import Formulario from './components/Formulario'
+import Anime from './components/Anime'
 
 function App() {
   const [personagens, setPersonagens] = useState([])
@@ -11,8 +12,11 @@ function App() {
     <div className="App">
       <Banner />
       <Formulario aoCadastrado={personagem => aoNovoCadastro(personagem)} />
+      <Anime nome="Attack on Titan" />
+      <Anime nome="Dragon Ball" />
+      <Anime nome="Hunter x Hunter" />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ organo/src/components/Anime/index.js
@@ -0,0 +1,11 @@
+import './Anime.css'
+
+const Anime = (props) =>{
+    return(
+        <section className="anime">
+            <h3>{props.nome}</h3>
+        </section>
+    )
+}
+
+export default Anime
